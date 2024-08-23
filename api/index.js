c8539606b7966755454c71b2d113e7f9857a46f7
Edwin Hernández <edwindesigner.br@gmail.com>
2024-08-23 14:42:49 -0400

    vercer y entonces?

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
-import app from "../";
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import signatureRoute from "../routes/signature.route.js";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/signature", signatureRoute);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Api signature" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default app;
